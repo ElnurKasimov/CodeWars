3b037fc97f6bf71572f415352d7d604ffeb3b670
ElnurKasimov <ekasimov2012@gmail.com>
2023-05-15 23:16:11 +0300

    added test for GenerateNames one more

--- src/test/java/Kju_7/ImageHostFilenameGeneratorTest.java
+++ src/test/java/Kju_7/ImageHostFilenameGeneratorTest.java
@@ -40,6 +40,16 @@ class ImageHostFilenameGeneratorTest {
     }
 
     @Test // TODO parametrized
+    public void testThatGenerateNameCreateNamesЦшерLettersOnly() {
+        //given when
+//        photoManager = new ImageHostFilenameGenerator.PhotoManager();
+//        Set<String> actual = photoManager.getNames();
+//        //then
+//        assertNotNull(actual);
+    }
+
+
+    @Test // TODO parametrized
     public void testThatNameWasUniqueWorkCorrect() {
         //given when
 //        photoManager = new ImageHostFilenameGenerator.PhotoManager();
