e6b7ced3d131a5b33d539e630c4a0e3876c349df
ElnurKasimov <ekasimov2012@gmail.com>
2023-05-21 22:40:47 +0300

    added getters and implemented Builder pattern

--- src/main/java/Kju_7/LombokBuilder.java
+++ src/main/java/Kju_7/LombokBuilder.java
@@ -18,11 +18,16 @@ package Kju_7;
 //        Encapsulate properly the class by providing read accessors (setters are not required for this Kata).
 //        Implement the Builder design Pattern.
 //        Additionally we have allegedly a JIT compiler and we want to do some micro-optimization by marking constants in the code.
+
 //        Constants
-//        In Java, constants are creating by adding the final keyword, but only primitives are true constants if we have objects that are not primitives marked as constants they actually have their memory location constant, not the values. It's pretty useful to optimize memory in JIT compilers since they can actually reserve memory before we use the variables and having the confidence in that their memory locations will not be changed through the scope.
+//        In Java, constants are creating by adding the final keyword, but only primitives are true constants
+//        if we have objects that are not primitives marked as constants they actually have their memory location constant, not the values.
+//        It's pretty useful to optimize memory in JIT compilers since they can actually reserve memory before we use the variables and having the confidence in that their memory locations will not be changed through the scope.
 //
 //        Builder Design Pattern
-//        This design pattern help us create instances of the object by specifying all of the data in (Dementor) chain of calls style in any order. The object creation is optimized by creating the instance only when the build method is called. If we call a method twice in the builder the value of the field will be the second setted.
+//        This design pattern help us create instances of the object by specifying all of the data in (Dementor) chain of calls style in any order.
+//        The object creation is optimized by creating the instance only when the build method is called.
+//        If we call a method twice in the builder the value of the field will be the second setted.
 //
 //        Note: If you don't use Lombok for this Kata ensure proper naming conventions are followed.
 
@@ -31,10 +36,14 @@ package Kju_7;
 public class LombokBuilder {
 
     public static class People{
-    public int age;
-    public String name;
-    public String lastName;
-    String GREET="hello";
+        private int age;
+        private String name;
+        private  String lastName;
+        private  final String GREET="hello";
+
+        public int getAge() { return this.age;};
+        public String getName() {return this.name;};
+        public String getLastName() {return  this.lastName;};
 
     public String greet(){
         return GREET+" my name is "+name;
