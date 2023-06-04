b3ab0f5a75720580388d91d6388b290f48ba3451
Elnur <ekasimov2012@gmail.com>
2023-06-04 22:07:29 +0300

    started testing of BouncingBall

---
+++ src/test/java/Kju_7/BouncingBallTest.java
@@ -0,0 +1,12 @@
+package Kju_7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BouncingBallTest {
+
+    @Test
+    void bouncingBall() {
+    }
+}
